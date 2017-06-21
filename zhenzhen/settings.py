e458c3a3c3d5f69d9e3cd672366c1a082c5e2ac2
lizhiwen <993859573@qq.com>
2017-06-21 19:29:27 +0800

    zhenzhen

--- zhenzhen/settings.py
+++ zhenzhen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1xs&ybermsrf14$bop-pn+dw@-)7$w+i%ga()l07we4_s-#o#*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'ubuntu ', '.zhenzhen.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'ubuntu ', '.zhenzhen.date']
 
 
 # Application definition
