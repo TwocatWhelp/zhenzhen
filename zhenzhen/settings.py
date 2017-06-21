d95573babf589860c635156a36e86d46e1b29f2a
lizhiwen <993859573@qq.com>
2017-06-21 18:04:01 +0800

    zhenzhen

--- zhenzhen/settings.py
+++ zhenzhen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1xs&ybermsrf14$bop-pn+dw@-)7$w+i%ga()l07we4_s-#o#*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'ubuntu ', '.zhenzhen.com']
 
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
